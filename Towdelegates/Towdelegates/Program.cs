a06829cae14a77a86870b49b63cf4417ecfc54f5
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-10 21:17:56 -0700

    init

---
+++ Towdelegates/Towdelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Towdelegates
+{
+    class Program
+    {
+
+        delegate void dele(string s);
+                    class TestClass
+                    {
+                    static void Good(string s)
+                    {
+                    System.Console.WriteLine("  Good, {0}!", s);
+                    }
+
+                    static void Morning(string s)
+                    {
+                    System.Console.WriteLine("  Morning, {0}!", s);
+                    }
+
+        static void Main()
+        {
+            dele firstdel, secondDel, multiDel, multiMinusfirstdel;
+            firstdel = Good;
+            secondDel = Morning;
+            multiDel = firstdel + secondDel;
+            multiMinusfirstdel = multiDel - firstdel;
+            Console.WriteLine("Invoking delegate firstdel:");
+            firstdel("A");
+            Console.WriteLine("Invoking delegate secondDel:");
+            secondDel("B");
+            Console.WriteLine("Invoking delegate multiDel:");
+            multiDel("C");
+            Console.WriteLine("Invoking delegate multiMinusfirstdel:");
+            multiMinusfirstdel("D");
+            Console.ReadLine();
+        }
+    }
+}
+}
